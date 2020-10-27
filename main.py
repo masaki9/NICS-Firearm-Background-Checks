63cbb54de35e2c63b25303cde658cb43e45fd99c
masaki <40445257+masaki9@users.noreply.github.com>
2020-10-27 01:33:12 -0700

    Updated each plot to include 2019

--- main.py
+++ main.py
@@ -52,7 +52,9 @@ def add_value_labels(ax, spacing=5, decimal=0):
 
 
 def plot_num_checks_recent_years():
-    # Create groups for years 2015 - 2018
+    # Create groups for years 2015 - 2019
+    df_y2019 = df.groupby('year').get_group(2019)\
+        .groupby('month')['totals'].sum().reset_index()
     df_y2018 = df.groupby('year').get_group(2018)\
         .groupby('month')['totals'].sum().reset_index()
     df_y2017 = df.groupby('year').get_group(2017)\
@@ -62,9 +64,11 @@ def plot_num_checks_recent_years():
     df_y2015 = df.groupby('year').get_group(2015)\
         .groupby('month')['totals'].sum().reset_index()
 
-    plt.figure(figsize=(12, 8))
+    plt.figure(figsize=(16, 12))
 
-    # Plot lines for years 2015 - 2018
+    # Plot lines for years 2015 - 2019
+    plt.plot(df_y2019['month'], df_y2019['totals'],
+             "--p", color='indianred', label="2019")
     plt.plot(df_y2018['month'], df_y2018['totals'],
              ":.", color='magenta', label="2018")
     plt.plot(df_y2017['month'], df_y2017['totals'],
@@ -75,9 +79,9 @@ def plot_num_checks_recent_years():
              "-*", markersize=4, color='bisque', label="2015")
 
     plt.ylim(ymin=0)  # Set y axis to start from 0
-    plt.xticks(df_y2018['month'], months)
+    plt.xticks(df_y2019['month'], months)
     plt.ylabel('Number of Checks')
-    plt.title('Total Number of Checks by Month (2015 - 2018)')
+    plt.title('Number of Firearm Background Checks by Month (2015 - 2019)')
     plt.legend(loc='best')
     add_thousands_separator_yaxis()
 
@@ -85,14 +89,15 @@ def plot_num_checks_recent_years():
 
 
 def plot_num_checks_by_type_recent_years():
-    # Total number of checks by month between 2016 and 2018 (inclusive)
-    df_recent_years = df[(df['year'].between(2016, 2018, inclusive=True))]\
+    # Total number of checks by month between 2015 and 2019 (inclusive)
+    df_recent_years = df[(df['year'].between(2015, 2019, inclusive=True))]\
         .groupby('month').sum().reset_index()
 
     plt.subplots(nrows=5, ncols=1, figsize=(15, 15))
 
     plt.subplot(5, 1, 1)
-    plt.title("Total Number of Checks by Month by Type (2016 - 2018)")
+    t = "Number of Firearm Background Checks by Month by Type (2015 - 2019)"
+    plt.title(t)
     plt.bar(df_recent_years['month'], df_recent_years['handgun'],
             color="lightpink", label='Handgun')
     plt.xticks(df_recent_years['month'], months)
@@ -158,13 +163,13 @@ def plot_top10_states_by_num_checks():
 
 
 def plot_num_checks_by_year():
-    df_total = df[(df['year'].between(1999, 2018, inclusive=True))]\
+    df_total = df[(df['year'].between(1999, 2019, inclusive=True))]\
         .groupby('year').sum().reset_index()\
         .rename(columns={'totals': '# of checks'})
 
     plt.figure(figsize=(20, 12))
     plt.margins(x=0.01)
-    plt.title("Number of Checks by Year (1999 - 2018)")
+    plt.title("Number of Firearm Background Checks by Year (1999 - 2019)")
     plt.ylabel("Number of Checks")
     plt.xticks(df_total['year'])
     plt.plot(df_total['year'], df_total['# of checks'], color="goldenrod")
